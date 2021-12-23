74a5ba7bc95b218e130384887128057c340c92ef
yyc-git <395976266@qq.com>
2021-12-23 10:49:00 +0800

    feat: add design_principle/

---
+++ packages/design_principle/need_refactor_example/need_refactor_code/Component.vue
@@ -0,0 +1,64 @@
+<template>
+  <section>
+    <h1>Global Data1 is {{ window.globalData1 }}</h1>
+    <button @click="createScene1()">创建场景1</button>
+    <button @click="createScene2AndSendSceneData()">
+      创建场景2并且发送场景数据
+    </button>
+  </section>
+</template>
+
+<script>
+import Vue from "vue";
+import {
+  Scene,
+  Mesh,
+  BoxGeometry,
+  MeshBasicMaterial,
+  SphereGeometry,
+} from "three";
+
+export default Vue.extend({
+  name: "Component",
+  methods: {
+    createScene1: function () {
+      let scene = new Scene();
+
+      let geometry = new BoxGeometry();
+      let material = new MeshBasicMaterial();
+
+      let cube = new Mesh();
+      cube.geometry = geometry;
+      cube.material = material;
+
+      scene.add(cube);
+    },
+    createScene2AndSendSceneData: function () {
+      let scene = new Scene();
+
+      let geometry = new BoxGeometry();
+      let material = new MeshBasicMaterial();
+
+      let cube = new Mesh();
+      cube.geometry = geometry;
+      cube.material = material;
+
+      geometry = new SphereGeometry();
+      material = new MeshBasicMaterial();
+      let sphere = new Mesh();
+      sphere.geometry = geometry;
+      sphere.material = material;
+
+      scene.add(cube);
+      scene.add(sphere);
+
+      fetch(serverUrl, scene).then((result) => {
+        console.log(result);
+      });
+    },
+  },
+});
+</script>
+
+<style>
+</style>
