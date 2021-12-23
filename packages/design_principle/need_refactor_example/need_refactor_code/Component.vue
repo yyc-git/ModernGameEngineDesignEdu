baf338c0cc47c27c8ef36346a27f8e8e6e6c72b1
yyc-git <395976266@qq.com>
2021-12-23 21:12:42 +0800

    feat: update design_principle/

--- packages/design_principle/need_refactor_example/need_refactor_code/Component.vue
+++ packages/design_principle/need_refactor_example/need_refactor_code/Component.vue
@@ -1,7 +1,9 @@
 <template>
   <section>
+    <!-- 不要用全局变量 -->
     <h1>Global Data1 is {{ window.globalData1 }}</h1>
     <button @click="createScene1()">创建场景1</button>
+    <!-- 一个函数做两件事情 -->
     <button @click="createScene2AndSendSceneData()">
       创建场景2并且发送场景数据
     </button>
@@ -10,6 +12,7 @@
 
 <script>
 import Vue from "vue";
+// 如何替换three.js为wonder而不影响ui代码？
 import {
   Scene,
   Mesh,
@@ -36,6 +39,8 @@ export default Vue.extend({
     createScene2AndSendSceneData: function () {
       let scene = new Scene();
 
+      // create cube重复代码
+
       let geometry = new BoxGeometry();
       let material = new MeshBasicMaterial();
 
@@ -52,6 +57,7 @@ export default Vue.extend({
       scene.add(cube);
       scene.add(sphere);
 
+      // 不应该在ui中直接与后端交互
       fetch(serverUrl, scene).then((result) => {
         console.log(result);
       });
